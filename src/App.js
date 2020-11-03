e99a2799e535faef3f2671a480688f554497a964
bosshogg86 <jeffhogg86@gmail.com>
2020-11-03 00:34:22 -0800

    adds material-ui and Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  // Link,
+  Switch,
+  // Redirect,
+} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
+    <>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Navbar />
+        <Switch>
+          {/* <Route path={"/about"} component={About} />
+          <Route path={"/projects"} component={Projects} />
+          <Route path={"/contact"} component={Contact} />
+          <Route path={"/resume"} component={Resume} />
+          <Route path={"/"} component={Home} /> */}
+        </Switch>
+        {/* <Footer /> */}
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  MenuItem,
+  Menu,
+  Hidden,
+} from "@material-ui/core";
+
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Portfolio
+          </Typography>
+          <Hidden only={["xs", "sm"]}>
+            <Button color="inherit" component={Link} to={"/about"}>
+              About
+            </Button>
+            <Button color="inherit" component={Link} to={"/projects"}>
+              Projects
+            </Button>
+            <Button color="inherit" component={Link} to={"/contact"}>
+              Contact
+            </Button>
+            <Button color="inherit" component={Link} to={"/resume"}>
+              Resume
+            </Button>
+          </Hidden>
+          <div>
+            <Hidden only={["md", "lg", "xl"]}>
+              <IconButton
+                aria-controls="menu"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Hidden>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem component={Link} to={"/"} onClick={handleClose}>
+                Home
+              </MenuItem>
+              <MenuItem component={Link} to={"/about"} onClick={handleClose}>
+                About
+              </MenuItem>
+              <MenuItem component={Link} to={"/projects"} onClick={handleClose}>
+                Projects
+              </MenuItem>
+              <MenuItem component={Link} to={"/contact"} onClick={handleClose}>
+                Contact
+              </MenuItem>
+              <MenuItem component={Link} to={"/resume"} onClick={handleClose}>
+                Resume
+              </MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
