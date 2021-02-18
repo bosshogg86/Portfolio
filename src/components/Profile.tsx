b29bb92c2fdaf7f9371011620da6205c9460d4fd
jeff <jeffhogg86@gmail.com>
2021-02-18 00:54:59 -0800

    remove paper, favicon background

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,8 +4,6 @@ import {
   Avatar,
   Box,
   Button,
-  Card,
-  CardContent,
   Container,
   Typography,
   makeStyles,
@@ -25,41 +23,43 @@ const useStyles = makeStyles(() => ({
     width: 250,
     marginBottom: 25,
   },
+  profile: {
+    alignItems: 'center',
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+  },
 }));
 
 const Profile = () => {
   const classes = useStyles();
 
   return (
-    <Card>
-      <CardContent>
-        <Box alignItems="center" display="flex" flexDirection="column">
-          <Avatar className={classes.avatar} src={avatar} />
-          <Typography color="textPrimary" gutterBottom variant="h3">
-            {user.name}
-          </Typography>
-          <Typography color="textSecondary" variant="body1">
-            {`${user.city}, ${user.state}`}
-          </Typography>
+    <Box className={classes.profile}>
+      <Avatar className={classes.avatar} src={avatar} />
+      <Typography color="textPrimary" gutterBottom variant="h3">
+        {user.name}
+      </Typography>
+      <Typography color="textSecondary" variant="body1">
+        {`${user.city}, ${user.state}`}
+      </Typography>
 
-          <Typography>
-            <Container>
-              <p>
-                Full-stack web developer currently working with:
-                <br />
-                Javascript | Python | React | Redux | TypeScript | Node.js |
-                Express | MongoDB | MySQL | Git | Heroku | AWS
-                <br />
-                Check out my portfolio!
-              </p>
-            </Container>
-          </Typography>
-          <Button variant="outlined" component={Link} to={'/portfolio'}>
-            Portfolio
-          </Button>
-        </Box>
-      </CardContent>
-    </Card>
+      <Typography>
+        <Container>
+          <p>
+            Full-stack web developer currently working with:
+            <br />
+            Javascript | Python | React | Redux | TypeScript | Node.js | Express
+            | MongoDB | MySQL | Git | Heroku | AWS
+            <br />
+            Check out my portfolio!
+          </p>
+        </Container>
+      </Typography>
+      <Button variant="outlined" component={Link} to={'/portfolio'}>
+        Portfolio
+      </Button>
+    </Box>
   );
 };
 
